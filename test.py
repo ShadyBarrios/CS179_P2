a3abf3c6e7dd34137f6f8758557a234acb4a58d7
Pineapple-Soup <91655764+Pineapple-Soup@users.noreply.github.com>
2025-11-14 20:36:33 -0800

    test: cluster assignment

--- test.py
+++ test.py
@@ -1,6 +1,5 @@
 from main import *
 
-# TODO: Test center calculation in `calculate_cluster_center`
 def test_calculate_cluster_center(center, radius, num_points):
     x, y = generate_circle_points(center, radius, num_points)
     cluster_coordinates = [Coordinate(x1, y1) for x1, y1 in zip(x, y)]
@@ -9,7 +8,6 @@ def test_calculate_cluster_center(center, radius, num_points):
     print(f"Calculated center: {calculated_center}")
     return calculated_center
 
-# TODO: Test SE calculation in `calculate_squared_error`
 def test_calculate_squared_error(center, radius, num_points, trials = 20):
     print(f"Expected OF Value: {(radius**2) * num_points}")
     total = 0
@@ -18,7 +16,28 @@ def test_calculate_squared_error(center, radius, num_points, trials = 20):
         cluster_coordinates = [Coordinate(x1, y1) for x1, y1 in zip(x, y)]
         total += calculate_squared_error(center, cluster_coordinates)
     print(f"Average OF over {trials} trials: {(total / trials):.3f}")
-# TODO: Test SSE calculation in `calculate_sum_squared_error`
+
+def test_cluster_assignment(centers, radius, num_points, colors):
+    all_x = []
+    all_y = []
+    coordinate_list = []
+    for center in centers:
+        x, y = generate_circle_points(center, radius, num_points)
+        all_x.append(x)
+        all_y.append(y)
+        cluster_coordinates = [Coordinate(x1, y1) for x1, y1 in zip(x, y)]
+        coordinate_list.extend(cluster_coordinates)
+    
+    assigned_x = [[] for i in range(len(centers))]
+    assigned_y = [[] for i in range(len(centers))]
+    for coordinate in coordinate_list:
+        idx = assign_coordinate_to_cluster(coordinate, centers)
+        assigned_x[idx].append(coordinate.get_x())
+        assigned_y[idx].append(coordinate.get_y())
+
+    plot_circles(centers, all_x, all_y, radius)
+    plot_circles(centers, assigned_x, assigned_y, radius, colors)
+
 
 if __name__ == "__main__":
     print("===TESTING CENTER FINDING===")
@@ -47,17 +66,6 @@ if __name__ == "__main__":
     test_calculate_squared_error(Coordinate(0, 0), 2, 128, trials=20)
     print()
 
-    # centers = [Coordinate(5, 5), Coordinate(5, 10)]
-    # radius = 1
-    # num_points = 50
-    # all_x = []
-    # all_y = []
-    # coordinates_list = []
-    # for center in centers:
-    #     x, y = generate_circle_points(center, radius, num_points)
-    #     coordinates = [Coordinate(x1, y1) for x1, y1 in zip(x, y)]
-    #     coordinates_list.append(coordinates)
-    #     all_x.append(x)
-    #     all_y.append(y)
-    
-    # plot_circles(centers, all_x, all_y, radius)
+    print("===TESTING CLUSTER ASSIGNMENT===")
+    print("First image is dataset, second is cluster assignment")
+    test_cluster_assignment([Coordinate(0, 0), Coordinate(5, 0)], 1, 64,  ["green", "orange"])
